7ea2f6033f5759837db00ca0190e695693cdd3e6
Douglas Saturnino <douglas02saturnino@gmail.com>
2024-03-06 17:50:33 -0300

    Dicce Challenger

--- javascript/Day 6/app.js
+++ javascript/Day 6/app.js
@@ -51,10 +51,20 @@ const delayedColorChange = (color, delay) => {
     });
 };
 
-delayedColorChange('red', 1000)
-    .then(() => delayedColorChange('orange', 1000))
-    .then(() => delayedColorChange('yellow', 1000))
-    .then(() => delayedColorChange('green', 1000))
-    .then(() => delayedColorChange('blue', 1000))
-    .then(() => delayedColorChange('indigo', 1000))
-    .then(() => delayedColorChange('violet', 1000));
+// delayedColorChange('red', 1000)
+//     .then(() => delayedColorChange('orange', 1000))
+//     .then(() => delayedColorChange('yellow', 1000))
+//     .then(() => delayedColorChange('green', 1000))
+//     .then(() => delayedColorChange('blue', 1000))
+//     .then(() => delayedColorChange('indigo', 1000))
+//     .then(() => delayedColorChange('violet', 1000));
+
+async function rainbow() {
+    await delayedColorChange('red', 1000);
+    await delayedColorChange('orange', 1000);
+    await delayedColorChange('yellow', 1000);
+    await delayedColorChange('green', 1000);
+    await delayedColorChange('blue', 1000);
+    await delayedColorChange('indigo', 1000);
+    await delayedColorChange('violet', 1000);
+}
