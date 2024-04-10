8ecafe026d5362a82745b554458a1df4f949d5fc
Douglas Saturnino <douglas02saturnino@gmail.com>
2024-04-10 10:49:41 -0300

    Mongo methods

---
+++ mongo/introduction/person.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/shopApp", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connection Open!!")
+    })
+    .catch(err => {
+        console.log("Oh no Error!!")
+        console.log(err)
+    })
+
+const productSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+})
+
+personSchema.virtual('fullName').get(function () {
+    return `${this.first} ${this.last}`
+})
+
+personSchema.pre('save', async function () {
+    this.first = "YO";
+    this.last = "MAMA";
+    console.log("About to save!!")
+})
+
+personSchema.post('save', async function () {
+    console.log("Just Saved!!")
+})
+
+
+const Person = mongoose.model('Person', personSchema);
