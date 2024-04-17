0d1a98f8e91014c9ba505fe5b902b211e1891c4b
Douglas Saturnino <douglas02saturnino@gmail.com>
2024-04-17 15:04:50 -0300

    API Delete

--- mongo/basic_setup/index.js
+++ mongo/basic_setup/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const Product = require('./models/product');
+const methodOverride = require('method-override');
 
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect('mongodb://localhost:27017/dogs', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log('Mongo Connection Open!');
@@ -14,10 +17,45 @@ mongoose.connect('mongodb://localhost:27017/dogs', { useNewUrlParser: true, useU
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
-app.get('/dog', (req, res) => {
-    res.send('Woof!');
+const categories = ['fruit', 'vegetable', 'dairy'];
+
+app.get('/product', async (req, res) => {
+    const products = await Product.find({});
+    console.log(products);
+    res.render('products/index', { products });
 });
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new', { categories });
+});
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+});
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    console.log(product);
+    res.render('products/show', { product });
+})
+
+app.get('/products/:id/edit', async(req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product, categories });
+});
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+    res.redirect(`/products/${product._id}`);
+})
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
