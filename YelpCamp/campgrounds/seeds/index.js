bbbeb35210887081dfab227ffedf5d8a18f4f481
Douglas Saturnino <douglas02saturnino@gmail.com>
2024-05-02 09:08:10 -0300

    YampComplete

--- YelpCamp/campgrounds/seeds/index.js
+++ YelpCamp/campgrounds/seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)}, ${sample(places)}`
+            title: `${sample(descriptors)}, ${sample(places)}`,
+            image: 'https://images.unsplash.com/photo-1714409117568-1c6f3aa3bc20',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti praesentium corporis fuga, mollitia eveniet odio corrupti voluptate aperiam, nam eaque, repellat consequatur quidem? Quod fugit dicta et dolorum quas repudiandae?',
+            price
         })
         await camp.save();
     }
